cb9f8b0bd2276e8bd2bee2270c02946c43f533b0
Sarah Cole <sarahcole@Sarahs-MacBook-Air-2.local>
2025-09-09 11:32:08 -0400

    added storage file and updated backend to reflect

---
+++ amplify/storage/resource.ts
@@ -0,0 +1,3 @@
+import { defineStorage } from "@aws-amplify/backend";
+
+export const storage = defineStorage({  name: "amplifyNotesDrive",  access: (allow) => ({    "media/{entity_id}/*": [      allow.entity("identity").to(["read", "write", "delete"]),    ],  }),});
